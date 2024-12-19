77d1f249e924ba4e09f1d1f8875e3b1257ce85b1
AkshayBoricha12 <akshay.maxah@gmail.com>
2024-12-19 16:02:00 +0530

    Pagination and responsive sidebar implemented

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let products = document.getElementById("products");
 let productForm = document.getElementById("product-form");
 let formContainer = document.querySelector(".form-container");
 let formInputs = document.querySelectorAll(".form-input");
+let sidebar = document.querySelector(".sidebar");
+let sidebarToggleBtn = document.querySelector(".sidebar-toggle-btn");
 let viewButtons = document.querySelectorAll(".view-btn");
 let sortingOptions = document.querySelectorAll(".sort-by");
 let productCollection = JSON.parse(localStorage.getItem("products")) || [];
 let id = productCollection.length + 1;
 let editingProductID = null;
+let currentPage = 1;
 
 function showListView() {
   if (viewButtons[1].classList.contains("active-view")) {
@@ -47,10 +50,12 @@ function setProduct(event) {
 
 document.addEventListener("DOMContentLoaded", () => {
   getData();
+  managePagination();
   document.querySelector(".add-btn").addEventListener("click", showHideForm);
   document
     .querySelector(".sort-btn")
     .addEventListener("click", toggleSortingOptions);
+  sidebarToggleBtn.onclick = toggleSidebar;
 });
 
 function fillTheForm(i) {
@@ -64,18 +69,24 @@ function fillTheForm(i) {
 
 function getData() {
   let myProducts = JSON.parse(localStorage.getItem("products"));
+  const productsPerPage = 10;
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = currentPage * productsPerPage;
+
   if (productCollection.length === 0) {
     products.innerHTML = `<h1>No products available. Add a product to get started!</h1>`;
     localStorage.removeItem("products");
   } else if (myProducts && myProducts.length > 0) {
     products.innerHTML = "";
-    for (const product of myProducts) {
+    const paginatedProducts = myProducts.slice(startIndex, endIndex);
+
+    for (const product of paginatedProducts) {
       let div = document.createElement("div");
-      div.classList += "product";
+      div.classList.add("product");
 
       div.innerHTML = `
               <div class="product-top">
-                <div class="product-id">1</div>
+                <div class="product-id">${product.id}</div>
                 <div class="product-image flex flex-center">
                   <img
                     src="${product.image}"
@@ -87,24 +98,20 @@ function getData() {
               <div class="product-middle">
                 <div class="product-details flex flex-column">
                   <div class="product-title">
-                    <h3 class="h3
-                     ">${product.name
-                       .split(" ")
-                       .map((element) => {
-                         return (
-                           element[0].toUpperCase() +
-                           element.slice(1).toLowerCase()
-                         );
-                       })
-                       .join(" ")}</h3>
+                    <h3 class="h3">${product.name
+                      .split(" ")
+                      .map(
+                        (element) =>
+                          element[0].toUpperCase() +
+                          element.slice(1).toLowerCase()
+                      )
+                      .join(" ")}</h3>
                   </div>
                   <div class="product-description flex">
-                    <p>
-                      ${
-                        product.description[0].toUpperCase() +
-                        product.description.slice(1).toLowerCase()
-                      }
-                    </p>
+                    <p>${
+                      product.description[0].toUpperCase() +
+                      product.description.slice(1).toLowerCase()
+                    }</p>
                   </div>
                   <div class="product-price">
                     <p>Price:&#8377;<span>${product.price}</span></p>
@@ -119,16 +126,14 @@ function getData() {
               </div>
               <div class="product-bottom flex">
                 <div class="product-bottom-left">
-                  <button class="btn edit-btn data-id="${
+                  <button class="btn edit-btn" data-id="${
                     product.id
                   }" onclick="fillTheForm(${product.id})">Edit</button>
                 </div>
                 <div class="product-bottom-right">
                   <button class="btn delete-btn" data-id="${
                     product.id
-                  }" onclick="deleteProduct(event)">
-                    Delete
-                  </button>
+                  }" onclick="deleteProduct(event)">Delete</button>
                 </div>`;
       products.appendChild(div);
     }
@@ -147,8 +152,10 @@ function deleteProduct(event) {
     let removeID = parseInt(event.target.getAttribute("data-id"));
     productCollection.splice(removeID - 1, 1);
     localStorage.setItem("products", JSON.stringify(productCollection));
+    productForm.reset();
     setID();
     getData();
+    managePagination();
   }
 }
 
@@ -174,37 +181,37 @@ function validateForm(event) {
   if (
     !/^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg))(\?.*)?$/i.test(image.trim())
   ) {
-    allInputsValid = manageFormInputErrors(0, "Please enter valid image URL.");
+    allInputsValid = manageFormInputErrors(1, "Please enter valid image URL.");
   }
 
-  if (!/^(?!^\d+$)[A-Za-z\d\s]+$/.test(name.trim())) {
-    allInputsValid = manageFormInputErrors(1, "Please enter a valid name.");
+  if (!/^[\w\s.,\-()&]+$/i.test(name.trim())) {
+    allInputsValid = manageFormInputErrors(2, "Please enter a valid name.");
   }
 
   if (description.trim().length < 3 || description.trim().length > 100) {
     allInputsValid = manageFormInputErrors(
-      2,
+      3,
       "Description length should be between 3 and 100."
     );
   }
 
   if (!/^\d+$/.test(price) || parseInt(price) <= 0) {
     allInputsValid = manageFormInputErrors(
-      3,
+      4,
       "Price must be a positive integer."
     );
   }
 
   if (!/^\d+$/.test(quantity) || parseInt(quantity) <= 0) {
     allInputsValid = manageFormInputErrors(
-      4,
+      5,
       "Quantity must be a positive integer."
     );
   }
 
   if (tags && !/^([\w\s]+,)*[\w\s]+$/.test(tags.trim())) {
     allInputsValid = manageFormInputErrors(
-      5,
+      6,
       "Tags must be comma-separated words."
     );
   }
@@ -287,4 +294,78 @@ function sortData(sortBy) {
     setID();
     getData();
   }
+}
+
+function toggleSidebar() {
+  if (getComputedStyle(sidebar).display === "none") {
+    sidebar.style.display = "block";
+    sidebarToggleBtn.innerHTML = `<i class="fa-solid fa-x"></i>`;
+  } else {
+    sidebar.style.display = "none";
+    sidebarToggleBtn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  }
+}
+
+function plusPage(n) {
+  gotoPage(currentPage + n);
+}
+
+function managePagination() {
+  let noOfProducts = productCollection.length;
+  const productsPerPage = 10;
+  let noOfPages = Math.ceil(noOfProducts / productsPerPage);
+
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > noOfPages) {
+    currentPage = noOfPages;
+  }
+
+  let paginationDiv = document.querySelector(".pagination");
+  if (!paginationDiv) {
+    paginationDiv = document.createElement("div");
+    paginationDiv.className = "pagination flex flex-center";
+    products.parentElement.appendChild(paginationDiv);
+  }
+
+  paginationDiv.innerHTML = `
+    <div class="previous-page-btn page-btn ${
+      currentPage === 1 ? "disabled" : ""
+    }" onclick="changePage(-1)">
+      <i class="fa-solid fa-less-than"></i>
+    </div>
+    ${managePages(noOfPages)}
+    <div class="next-page-btn page-btn ${
+      currentPage === noOfPages ? "disabled" : ""
+    }" onclick="changePage(1)">
+      <i class="fa-solid fa-greater-than"></i>
+    </div>`;
+}
+
+function managePages(totalPages) {
+  let str = "";
+  for (let i = 1; i <= totalPages; i++) {
+    str += `<div class="page-btn ${
+      currentPage === i ? "active-page" : ""
+    }" onclick="gotoPage(${i})">${i}</div>`;
+  }
+  return str;
+}
+
+function changePage(direction) {
+  currentPage += direction;
+
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > Math.ceil(productCollection.length / 10)) {
+    currentPage = Math.ceil(productCollection.length / 10);
+  }
+  getData();
+  managePagination();
+}
+
+function gotoPage(page) {
+  currentPage = page;
+  getData();
+  managePagination();
 }
